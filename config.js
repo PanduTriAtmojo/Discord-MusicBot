46f6868d12d1d81b9a3527ccd730b35d6fb9a8dd
PanduTriAtmojo <123941242+PanduTriAtmojo@users.noreply.github.com>
2024-06-08 13:42:50 +0700

    Update config.js

--- config.js
+++ config.js
@@ -27,7 +27,7 @@ module.exports = {
 		{
 			identifier: "Main Node", //- Used for indentifier in stats commands.
 			host: 0.0.0.0, //- The host name or IP of the lavalink server.
-			port: 80, // The port that lavalink is listening to. This must be a number!
+			port: 2333, // The port that lavalink is listening to. This must be a number!
 			password: "", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
